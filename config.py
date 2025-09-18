56fd69fbfab907a746623b3b574996d59aefe0aa
Ilgara Yusifzada <ilgarayusifzada@gmail.com>
2025-09-18 22:35:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     CHUNK_SIZE = 1000
-    CHUNK_OVERLAP = 200
+    CHUNK_OVERLAP = 100
     TOP_K_CHUNKS = 3
     EMBEDDING_MODEL = "all-MiniLM-L6-v2"
     DB_PATH = "rag_database"
