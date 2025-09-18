9a1a185d1d3781c1b2a264c64f42c27562d6c109
Ilgara Yusifzada <ilgarayusifzada@gmail.com>
2025-09-18 07:53:43 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+@dataclass
+class Config:
+    CHUNK_SIZE = 1000
+    CHUNK_OVERLAP = 200
+    TOP_K_CHUNKS = 3
+    EMBEDDING_MODEL = "all-MiniLM-L6-v2"
+    LLAMA_MODEL = "microsoft/DialoGPT-medium"  # Lighter alternative, can be changed to full LLaMA
+    DB_PATH = "rag_database"
+    METADATA_FILE = "file_metadata.json"
+
+config = Config()
